c50d6ff147875d1e62779fd4bf2e004e3f6fa73c
Harutyun_Petrosyan <harut.k.petrosyan@gmail.com>
2021-09-23 21:53:15 +0400

    newTask

--- src/main/java/pageObject/MainPage.java
+++ src/main/java/pageObject/MainPage.java
@@ -1,9 +1,13 @@
 package pageObject;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPage {
     WebDriver driver;
@@ -12,27 +16,56 @@ public class MainPage {
         PageFactory.initElements(driver,this);
         this.driver=driver;
     }
-    @FindBy(id = "email")
-    private WebElement emailField;
-    @FindBy(id = "password")
-    private WebElement passwordField;
-    @FindBy(css ="button[class='btn btn-success btn-block']")
-    private WebElement signInButton;
-    @FindBy(xpath = "//span[text()='Users']")
-    private WebElement usersButton;
-    @FindBy(css = "a[href='/users-management']")
-    private WebElement usersManagementButton;
+    @FindBy(css = "button[data-role='gdpr-accept']")
+    private WebElement acceptCookieButton;
+    @FindBy(css = "img[class=\"btn-close\"]")
+    private WebElement closeAdvertisementButton;
+    @FindBy(css = "input[name='SearchText']")
+    private WebElement searchField;
+    @FindBy(css = "input[class='search-button']")
+    private WebElement searchButton;
+    @FindBy(css = "input[class=\"next-checkbox-input\"]")
+    private List<WebElement> checkboxes;
+    @FindBy(css = "div[class=\"_1OUGS\"] img")
+    private List<WebElement> images;
 
-
-
-    public void signIn(String email,String password){
-        emailField.sendKeys(email);
-        passwordField.sendKeys(password);
-        signInButton.click();
+    public void openPage(String url){
+        driver.get(url);
     }
-    public void navigateUsersManagementPage(){
-        usersButton.click();
-        usersManagementButton.click();
+
+    public void acceptCookies(){
+        try {
+            acceptCookieButton.click();
+            closeAdvertisementButton.click();
+        }catch (NoSuchElementException e){e.printStackTrace();}
     }
+    public void searchItem(String itemName){
+        searchField.sendKeys(itemName);
+        searchButton.click();
+    }
+    public void chooseFreeShippingOptions(){
+        checkboxes.get(0).click();
+    }
+    public void clickOnThirdItem(){
+        images.get(2).click();
+    }
+    public void changeToNewTab(){
+        ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(newTab.get(1));
+    }
+
+
+    public void takeScreenShot(WebDriver webdriver, String fileWithPath) throws Exception {
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile = new File(fileWithPath.replace("\\", "/"));
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+
+
+
+
+
 
 }
